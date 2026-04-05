12115a588eb05cf154645e5d4b8fa86a3d01d9fe
Yuki Motokane <216834584+motokaneyuki@users.noreply.github.com>
2026-04-05 19:08:38 +0900

    feat: add function to display library books

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
-const myLibrary = [];
+const container = document.querySelector('.container');
+
+const sampleBook1 = new Book('James Arthur', 'To Be A Swan', 120, 'Read', crypto.randomUUID());
+const sampleBook2 = new Book('Peter Artemis', 'I Want To Dance', 300, 'Reading', crypto.randomUUID());
+
+const myLibrary = [sampleBook1, sampleBook2];
 
 function Book(author, title, pages, read, id) {
     if (!new.target){
@@ -17,3 +22,33 @@ function addBookToLibrary(author, title, pages, read) {
     const book = new Book(author, title, pages, read, id);
     return myLibrary.push(book);
 }
+
+function displayBooks() {
+    for(let i = 0; i < myLibrary.length; i++){
+        const card = document.createElement('div');
+        card.classList.add('card');
+        container.appendChild(card);
+
+        const title = document.createElement('div');
+        title.innerText = myLibrary[i].title.toUpperCase();
+        title.classList.add('title');
+        card.appendChild(title);
+
+        const author = document.createElement('div');
+        author.innerText = 'by ' + myLibrary[i].author;
+        author.classList.add('author');
+        card.appendChild(author);
+
+        const pages = document.createElement('div');
+        pages.innerText = 'Pages: ' + myLibrary[i].pages;
+        pages.classList.add('pages');
+        card.appendChild(pages);
+
+        const read = document.createElement('div');
+        read.innerText = 'Status: ' + myLibrary[i].read;
+        read.classList.add('read');
+        card.appendChild(read);
+    }
+}
+
+displayBooks();
